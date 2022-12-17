54e3ee0a8a506a3de90fada9970cfb828b4d3d12
kskaranvir <87475752+kskaranvir@users.noreply.github.com>
2022-12-18 02:54:06 +0530

    Add files via upload

---
+++ assignment_1/Main.java
@@ -0,0 +1,54 @@
+package assignment_1;
+
+import java.util.*;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		Functions obj = new Functions();
+		
+		//----------- Question(1) -----------
+		System.out.print("Enter a string: ");  
+		String str= sc.nextLine(); 
+		System.out.print("Sorted string : ");
+		obj.sort(str.toCharArray());
+		//------------------------------------
+		
+		sc.close();
+		System.out.println();
+		
+		//----------- Question(2) ------------
+		try{  
+			Functions.Student s1=obj.new Student(3154,"Karan");  
+			  
+			Functions.Student s2=(Functions.Student)s1.clone();  
+			  
+			System.out.println(s1.rollno+" "+s1.name);  
+			System.out.println(s2.rollno+" "+s2.name);  
+			  
+			}catch(CloneNotSupportedException c){} 
+		//------------------------------------
+		
+		//----------- Question(3) ------------
+		obj.for_to_while();
+		//------------------------------------
+				
+		//----------- Question(4(i)) ------------
+//		@SuppressWarnings("unused")
+		Functions.Child innerObj = obj.new Child();
+		------------------------------------	
+		
+		System.out.println();
+		
+		//----------- Question(4(ii)) ------------
+		obj.ids();
+		//------------------------------------
+		
+		//----------- Question(5) ------------
+		obj.formatted_output();
+		//------------------------------------
+		
+		//----------- Question(6) ------------
+		obj.substring();
+		//------------------------------------
+	}
+}
